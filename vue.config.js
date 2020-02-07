fe845757b1ca77545350c88486dbe2a2ec20fab4
Kartik Derasari <derasarikartik@gmail.com>
2020-02-07 20:44:00 +0530

    initial commit

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+/* module.exports = {
+  baseUrl: './',
+
+  pwa: {
+    name: 'Speckle',
+    themeColor: '#0962FF'
+  },
+
+  devServer: {
+    proxy: {
+      '^/.netlify/functions/*': {
+        target: 'http://localhost:9000',
+        pathRewrite: function (path, req) { return path.replace('/.netlify/functions', '') },
+        changeOrigin: true
+      }
+    }
+  }
+}
+ */
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/arkeytec-dashboard/'
+    : '/'
+}
